6380f8621747067b6b08ab6f615519118f6d69da
Harsh Sahu <harshsahu.8085@gmail.com>
2023-08-22 11:14:38 +0200

    FDP and ODP Functions

--- GCDP_function_script.R
+++ GCDP_function_script.R
@@ -1,9 +1,19 @@
+########### PACKAGES ######################
 getwd()
+library(tidyverse)
+library(psych)
+library(dplyr)
+library(writexl)
+library(readr)
+library(vroom)
+library(lubridate)
+library(purrr)
 library(data.table)
 
-GCDP <- function(input_path, start_datetime, end_datetime, output_path) {
+######## FTIR FUNCTION #########
+FDP <- function(raw_path, clean_path) {
   # Read the TXT file using data.table's fread function
-  data <- fread(input_path, header = TRUE)
+  data <- fread(raw_path, header = TRUE, fill = TRUE)
   
   # Filter out non-numeric rows in Messstelle column
   data <- data[grepl("^\\d+$", Messstelle)]
@@ -22,11 +32,8 @@ GCDP <- function(input_path, start_datetime, end_datetime, output_path) {
     as.numeric(CH4),
     as.numeric(H2O))]
   
-  # Filter data based on start_datetime and end_datetime
-  data <- data[DateTime >= start_datetime & DateTime <= end_datetime]
-  
   # Create the output file name
-  output_file <- file.path(output_path, paste0("GC_", format(Sys.Date(), "%Y%m%d"), ".CSV"))
+  output_file <- file.path(clean_path, paste0(format(Sys.Date(), "%Y%m%d"), "_FDP", ".CSV"))
   
   # Write the processed data to the new CSV file
   write.csv(data, file = output_file, row.names = FALSE)
@@ -34,14 +41,51 @@ GCDP <- function(input_path, start_datetime, end_datetime, output_path) {
   cat("Processed data has been saved as", output_file, "\n")
 }
 
+# Call the function with the inputs (Example)
+#FDP(raw_path="D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/20230203_FTIR.TXT", clean_path="D:/Data Analysis/Gas_data/Clean_data/FTIR_clean")
 
-# Call the function with the inputs
-#GCDP(input_path, start_datetime, end_datetime, output_path)
 
 
-#Example
-GCDP("D:/Data Analysis/Master Thesis data/Ansyco FTIR Data/Ansyco_Data/Harsh vertical pipe setup.TXT",
-     "2021-09-02 11:40:45",
-     "2021-09-05 14:06:50",
-     "D:/Data Analysis/OTICE_gas_calibration/2023_FTIR_data")
+######### OTICE FUNCTION ##########
+ODP <- function(raw_path, clean_path) {
+  # Create an empty data frame
+  OTICE_data <- data.frame()
+  
+  # Get a list of CSV files in the directory
+  csv_files <- list.files(path = raw_path, pattern = "\\.csv$", full.names = TRUE)
+  
+  # Iterate over each CSV file, read its contents, and append to the OTICE_data data frame
+  for (file in csv_files) {
+    delimiter <- ifelse(grepl(";", readLines(file, n = 1)), ";", "\t")  # Determine the delimiter based on the first line of each file
+    
+    data <- read.table(file, header = TRUE, sep = delimiter)  # Adjust the sep parameter based on the delimiter
+    
+    # Select specific columns by index and rename column 1 to "DateTime"
+    selected_data <- data[, c(1, 2, 3, 4, 6, 10, 13)]
+    colnames(selected_data)[1] <- "DateTime"
+    
+    # Remove decimal points from seconds in the "DateTime" column
+    selected_data$DateTime <- sub("\\.\\d+", "", selected_data$DateTime)
+    
+    # Convert the "DateTime" column to a POSIXct object
+    selected_data$DateTime <- as.POSIXct(selected_data$DateTime, format = "%Y-%m-%d %H:%M:%S")
+    
+    # Adjust the time by adding 2 hours (120 minutes)
+    selected_data$DateTime <- selected_data$DateTime + minutes(120)
+    
+    OTICE_data <- rbind(OTICE_data, selected_data)
+  }
+  
+  # Create the output file name
+  output_file <- file.path(clean_path, paste0(format(Sys.Date(), "%Y%m%d"), "_ODP", ".CSV"))
+  
+  # Write the processed data to the new CSV file
+  write.csv(OTICE_data, file = output_file, row.names = FALSE)
+  
+  cat("Processed data has been saved as", output_file, "\n")
+}
+
+
+# Call the function to combine and save the data (Example)
+# ODP(raw_path="D:/Data Analysis/Gas_data/Raw_data/OTICE_raw", clean_path="D:/Data Analysis/Gas_data/Clean_data/OTICE_clean")
 
