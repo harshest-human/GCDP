7e9826f1229b9f9574afa0556a9627c3d662bd44
Harsh Sahu <harshsahu.8085@gmail.com>
2023-08-22 08:43:37 +0200

    adding output_path

--- GCDP_function_script.R
+++ GCDP_function_script.R
@@ -1,8 +1,9 @@
+getwd()
 library(data.table)
 
-GCDP <- function(file_path, start_datetime, end_datetime) {
+GCDP <- function(file_path, start_datetime, end_datetime, output_path) {
   # Read the TXT file using data.table's fread function
-  data <- fread(file_path, header = TRUE)
+  data <- fread(cmd = paste0('"', file_path, '"'), header = TRUE)
   
   # Filter out non-numeric rows in Messstelle column
   data <- data[grepl("^\\d+$", Messstelle)]
@@ -25,7 +26,7 @@ GCDP <- function(file_path, start_datetime, end_datetime) {
   data <- data[DateTime >= start_datetime & DateTime <= end_datetime]
   
   # Create the output file name
-  output_file <- paste0("GC_", format(Sys.Date(), "%Y%m%d"), ".CSV")
+  output_file <- file.path(output_path, paste0("GC_", format(Sys.Date(), "%Y%m%d"), ".CSV"))
   
   # Write the processed data to the new CSV file
   write.csv(data, file = output_file, row.names = FALSE)
@@ -34,8 +35,13 @@ GCDP <- function(file_path, start_datetime, end_datetime) {
 }
 
 
+# Call the function with the inputs
+#GCDP(input_path, start_datetime, end_datetime, output_path)
+
+
 #Example
-GCDP(file_path = "D:/Data Analysis/Master Thesis data/Ansyco FTIR Data/Ansyco_Data/Harsh vertical pipe setup.TXT",
+GCDP("D:/Data Analysis/Master Thesis data/Ansyco FTIR Data/Ansyco_Data/Harsh vertical pipe setup.TXT",
      "2021-09-02 11:40:45",
-     "2021-09-05	14:06:50")
+     "2021-09-05 14:06:50",
+     "D:/Data Analysis/OTICE_gas_calibration/2023_FTIR_data")
 
